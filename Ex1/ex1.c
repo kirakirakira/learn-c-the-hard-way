355425e0bb4123902b1e0f77ddac8dcbf1ef6eab
Kira Awesome <kembrekl@gmail.com>
2015-11-26 18:24:44 +0000

    added folders for each lesson

---
+++ Ex1/ex1.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/* This is a comment. */
+
+int main(int argc, char *argv[])
+{
+    int distance = 100;
+    
+    int multiplier = 3;
+    
+    // this is also a comment
+    printf("You are %d miles away.\n", distance);
+    
+    printf("I bet you are %d times as hungry as me.\n", multiplier);
+    
+    puts("I don't really care");
+    
+    puts("Why not?");
+    
+    puts("Just because");
+    
+    return 0;
+}
---
+++ Ex10/ex10ec3.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    int i = 1;
+    
+    for (i = 1; i < argc; i++) {
+        printf("%s\n", argv[i]);
+        int j = 0;
+        char letter = 0;
+        for (j = 0; letter = argv[i][j], letter != '\0'; j++) {
+            
+            switch(letter) {
+                case 'a':
+                case 'A':
+                    printf("%d: 'A'\n", j);
+                    break;
+                
+                case 'e':
+                case 'E':
+                    printf("%d: 'E'\n", j);
+                    break;   
+                
+                case 'i':
+                case 'I':
+                    printf("%d: 'I'\n", j);
+                    break;
+                    
+                case 'o':
+                case 'O':
+                    printf("%d: 'O'\n", j);
+                    break;
+                    
+                case 'u':
+                case 'U':
+                    printf("%d: 'U'\n", j);
+                    break;
+                    
+                case 'y':
+                case 'Y':
+                    if (j > 2) {
+                        // it's only sometimes Y
+                        printf("%d: 'Y'\n", j);
+                    }
+                    break;
+                    
+                default:
+                    printf("%d: %c is not a vowel\n", j, letter);
+            }
+        }
+    }
+    return 0;
+}
